8f78ed37f5007d96b05c5ac118023df8f3f9e71f
xretic-svg <xretic09@gmail.com>
2026-04-29 04:01:03 +0300

    Create app_export.dart

---
+++ lib/core/app_export.dart
@@ -0,0 +1,7 @@
+export 'package:flutter/material.dart';
+export 'package:cached_network_image/cached_network_image.dart';
+export 'package:sizer/sizer.dart';
+export '../widgets/custom_image_widget.dart';
+export '../widgets/custom_icon_widget.dart';
+export '../theme/app_theme.dart';
+export '../routes/app_routes.dart';
